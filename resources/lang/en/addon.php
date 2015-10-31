96dcbd1bfc74bcad7f656119790aed57903f2a16
Ryan Thompson <ryan@pyrocms.com>
2015-10-31 11:14:22 -0500

    Done

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -3,5 +3,5 @@
 return [
     'title'       => 'User Security Check',
     'name'        => 'User Security Check Extension',
-    'description' => ''
+    'description' => 'A security check for the Users module that assures users are active, enabled, and authorized.'
 ];
