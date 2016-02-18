58d5c2a0f26b3e98b4cd0d8d7e7e2431c75abcef
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 00:37:08 -0600

    Author

--- src/Command/CheckUser.php
+++ src/Command/CheckUser.php
@@ -9,9 +9,9 @@ use Illuminate\Routing\Redirector;
 /**
  * Class CheckUser
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\UserSecurityCheckExtension\Command
  */
 class CheckUser implements SelfHandling
--- src/UserSecurityCheckExtension.php
+++ src/UserSecurityCheckExtension.php
@@ -8,9 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * Class UserSecurityCheckExtension
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\UserSecurityCheckExtension
  */
 class UserSecurityCheckExtension extends SecurityCheckExtension
